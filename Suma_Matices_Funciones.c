9ee2b10cf83e9304bb6310fdcc1b3a5359b96109
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2023-12-03 23:40:05 -0500

    Correcion de la sintaxis y de errores del programa

--- Suma_Matices_Funciones.c
+++ Suma_Matices_Funciones.c
@@ -3,35 +3,43 @@
 #include <time.h>
 
 // Función para ingresar dimensiones de la matriz
-void ingresarDimensiones(int filas, int columnas) {
+void ingresarDimensiones(int filas, int columnas)
+{
     printf("Ingrese el valor para filas: ");
     scanf("%d", filas);
     printf("Ingrese el valor para columnas: ");
     scanf("%d", columnas);
 }
 // Función para generar y mostrar una matriz aleatoria
-void generarYMostrarMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+void generarYMostrarMatriz(int filas, int columnas, int matriz[filas][columnas])
+{
     printf("La matriz es: \n");
-    for (int i = 0; i < filas; i++) {
+    for (int i = 0; i < filas; i++)
+    {
         printf("\n");
-        for (int j = 0; j < columnas; j++) {
+        for (int j = 0; j < columnas; j++)
+        {
             matriz[i][j] = rand() % 101;
             printf("%d\t", matriz[i][j]);
         }
     }
 }
 // Función para sumar dos matrices
-void sumarMatrices(int filas, int columnas, int matriz1[filas][columnas], int matriz2[filas][columnas], int resultado[filas][columnas]) {
+void sumarMatrices(int filas, int columnas, int matriz1[filas][columnas], int matriz2[filas][columnas], int resultado[filas][columnas])
+{
     printf("\n\nEl resultado de la suma de las matrices es : \n ");
-    for (int i = 0; i < filas; i++) {
+    for (int i = 0; i < filas; i++)
+    {
         printf("\n");
-        for (int j = 0; j < columnas; j++) {
+        for (int j = 0; j < columnas; j++)
+        {
             resultado[i][j] = matriz1[i][j] + matriz2[i][j];
             printf("%d\t", resultado[i][j]);
         }
     }
 }
-int main() {
+int main()
+{
     // Inicializamos el generador de números aleatorios diferentes en cada ejecución
     srand(time(NULL));
 
