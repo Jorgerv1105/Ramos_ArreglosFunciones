8dc68eef897f893afd05a69ea7bc1908880a63a0
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2023-12-03 23:41:21 -0500

    Ingreso de bibliotecas y la primera funcion

---
+++ Matriz_Tridimensional_Funciones.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Función para ingresar dimensiones de la matriz tridimensional
+void ingresarDimensiones(int x, int n, int m)
+{
+    printf("Coloque el numero de matrices: \n");
+    scanf("%d", x);
+    printf("Coloque el numero de filas: \n");
+    scanf("%d", n);
+    printf("Coloque el numero de columnas:\n");
+    scanf("%d", m);
+}
